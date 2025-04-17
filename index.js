bd201623cf984dc491637d7df9026a7459252056
Mandresy <hei.mandresy.3@gmail.com>
2025-04-17 22:02:06 +0200

    Ajout des petites particule dans index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+window.onload = () => {
+    Particles.init({
+      selector: '.background',
+      color: ['#ea7af4', '#14213d'],
+      connectParticles: false,
+      maxParticles: 100,
+      speed: 1,
+    })
+};
